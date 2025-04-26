5f1cd0652546578cf41a3002c99be4e901ad7b76
mani rahmati <astrroeffect@gmail.com>
2025-04-26 20:10:59 +0330

    Initial commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { React } from 'react'
 import { IoHomeOutline, IoCallOutline } from "react-icons/io5";
 import { MdOutlineWorkOutline } from "react-icons/md";
 import { TbInfoTriangle } from "react-icons/tb";
+import { Link } from 'react-router-dom';
 
 import 'animate.css';
 import './Navbar.css';
@@ -10,10 +11,10 @@ export default function Navbar() {
     return (
         <nav className='animate__animated animate__fadeInUp'>
             <ul className='navbar'>
-                <li><a href="/"><IoHomeOutline className='navbar-icon' /></a></li>
-                <li><a href="/about"><TbInfoTriangle className='navbar-icon' /></a></li>
-                <li><a href="/contact"><IoCallOutline className='navbar-icon' /></a></li>
-                <li><a href="/services"><MdOutlineWorkOutline className='navbar-icon' /></a></li>
+                <li><Link to="/"><IoHomeOutline className='navbar-icon' /></Link></li>
+                <li><Link to="/about"><TbInfoTriangle className='navbar-icon' /></Link></li>
+                <li><Link to="/contact"><IoCallOutline className='navbar-icon' /></Link></li>
+                <li><Link to="/services"><MdOutlineWorkOutline className='navbar-icon' /></Link></li>
             </ul>
         </nav>
     )
