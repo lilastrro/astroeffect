9724810983d2ac26b1d6a7907ee10575cf0fd1ba
mani rahmati <astrroeffect@gmail.com>
2025-04-26 20:25:36 +0330

    Update React code

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import Home from './pages/home/home'
 import Services from './pages/services/services'
 import About from './pages/about/about'
 import Contact from './pages/contact/contact'
+import { Navigate } from 'react-router-dom'
 
 let routes = [
     { path: '/', element: <Home /> },
     { path: '/services', element: <Services /> },
     { path: '/about', element: <About /> },
     { path: '/contact', element: <Contact /> },
+    { path: '*', element: <Navigate to="/" /> }
 ]
 
 export default routes
